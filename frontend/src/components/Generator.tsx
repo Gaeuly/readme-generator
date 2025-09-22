9964011d3d25a2f480f0d30e4ec7bd3a984253a5
Gaeul <143046394+Gaeuly@users.noreply.github.com>
2025-09-22 10:46:37 +0700

    Update Generator.tsx

--- frontend/src/components/Generator.tsx
+++ frontend/src/components/Generator.tsx
@@ -54,7 +54,7 @@ const Generator: React.FC = () => {
 
     try {
       // Alamat backend kita (pastikan backend berjalan di port 5001)
-      const backendUrl = 'http://localhost:5001/api/generate';
+      const backendUrl = 'readme-generator-production-08d5.up.railway.app/api/generate';
 
       const response = await fetch(backendUrl, {
         method: 'POST',
